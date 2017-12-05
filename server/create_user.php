957aab72da21c076a97dcfe864621e26edf8522f
Alejandro Reyes <diego_1394_rg@hotmail.com>
2017-12-04 20:09:17 -0600

    Agregar usuario
    Este es la parte para que un usuario nuevo se pueda registrar para
    posteriormente poder ingresar al sistema

--- server/create_user.php
+++ server/create_user.php
@@ -1,7 +1,33 @@
 <?php
 
+    include('conector.php');
+    
+    $data['Nombre'] = "'".$_POST['Nombre']."'";
+    $data['Apellidos'] = "'".$_POST['Apellidos']."'";
+    $data['Correo'] = "'".$_POST['Correo']."'";
+    $data['Contrasena'] = "'".password_hash($_POST['Contrasena'], PASSWORD_DEFAULT)."'";
+    $data['Fecha_Nacimiento'] = "'".$_POST['Fecha_Nacimiento']."'";
 
+    $con = new ConectorBD('localhost','root','123456');
+    $response['conexion'] = $con->initConexion('agenda');
 
+    if ($response['conexion']=='OK') 
+    {
+        if($con->insertData('usuarios', $data))
+        {
+            $response['msg']="Se a Registrado con Exito el usuario";
+        }
+        else 
+        {
+            $response['msg']= "Hubo un error y los datos no han sido guardados";
+        }
+    }
+    else 
+    {
+        $response['msg']= "No se pudo conectar a la base de datos";
+    }
+
+    echo json_encode($response);
 
 
  ?>
